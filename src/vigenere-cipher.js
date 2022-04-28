82bf4806dfcb05d6c8f54799784c047e83fc6144
Volkov Danil <snake1234567890-1@yandex.ru>
2022-04-28 16:41:16 +0300

    feat: make Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(normal = true) {
+    this.reverse = !normal;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  letter = 65;
+  alphabet = [...new Array(26)].map(() => this.letter++);
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    if(this.reverse) {
+      message = message.split('').reverse().join('');
+      key = key.split('').reverse().join('');
+    }
+    while (key.length < message.length) {
+      key += key;
+    }
+    let i = 0
+    return message.split('').map((l) => {
+      if (!this.alphabet.includes(l.charCodeAt(0))) {
+        return l;
+      }
+      return String.fromCharCode(this.alphabet[(l.charCodeAt(0) + key.charCodeAt(i++)) % this.alphabet.length])
+    }).join('');
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    if(this.reverse) {
+      encryptedMessage = encryptedMessage.split('').reverse().join('');
+      key = key.split('').reverse().join('');
+    }
+    
+    while (key.length < encryptedMessage.length) {
+      key += key;
+    }
+    let i = 0;
+    return encryptedMessage.split('').map((l) => {
+      if (!this.alphabet.includes(l.charCodeAt(0))) {
+        return l;
+      }
+    
+      let temp = l.charCodeAt(0) - key.charCodeAt(i++);
+      let index = temp < 0 ? temp + this.alphabet.length : temp;
+      return String.fromCharCode(this.alphabet[index % this.alphabet.length])
+    }).join('');
   }
 }
 
